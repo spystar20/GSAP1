836c6cc4d1eb95c52f0adb81f1139ef9686037c8
spystar20 <hert5179@gmail.com>
2026-05-22 21:25:35 +0530

    text-reveal

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,6 +12,7 @@ tl.from(".hero",{
     start:'top top',end:'+=100%',trigger:'.hero',scrub:true
   }
 })
+
 ,gsap.from(".card",{
     y:100,opacity:0,stagger:0.3,duration:1,ease:'power3.inOut',scrollTrigger:{
         start:'top 80%',
@@ -26,6 +27,10 @@ gsap.to(cardSection,{
     x:-ScrollWidth,opacity:1,scrollTrigger:{
         trigger:'.horizontal-section',pin:true,scrub:true,start:'top top',end:`+=${ScrollWidth}`
     }
+}),
+gsap.from('.title',{
+    yPercent:100,duration:3, ease:'power3.inOut',scrollTrigger:{trigger:'.title',start:'top 80%',markers :true
+    }
 })
 },
  
@@ -38,7 +43,11 @@ gsap.to(cardSection,{
     <div class="w-full h-screen">
 <img src="https://i1-e.pinimg.com/webp85/1200x/fb/37/42/fb3742598694224cd9eff596195b6a57.webp" class="hero object-cover w-full h-screen" alt="">
     </div>
- 
+   <div class="overflow-hidden py-6">
+    <h2 class="title text-4xl font-semibold text-center" >
+        PORTFOLIO
+    </h2>
+    </div>
 <div class="horizontal-section h-screen overflow-hidden">
     <div class="cards-wrapper flex w-max gap-5 p-20 ">
         
@@ -49,6 +58,7 @@ gsap.to(cardSection,{
             <img class="card w-[500px] h-[400px]  border border-gray-400/30 rounded-3xl p-3" src="https://i1-e.pinimg.com/webp85/1200x/fb/37/42/fb3742598694224cd9eff596195b6a57.webp" alt="">   <img class="card w-[500px] h-[400px]  border border-gray-400/30 rounded-3xl p-3" src="https://i1-e.pinimg.com/webp85/1200x/fb/37/42/fb3742598694224cd9eff596195b6a57.webp" alt="">   <img class="card w-[500px] h-[400px]  border border-gray-400/30 rounded-3xl p-3" src="https://i1-e.pinimg.com/webp85/1200x/fb/37/42/fb3742598694224cd9eff596195b6a57.webp" alt="">
         </div>
     </div>
-    
+
+  
 </template>
 <style scoped></style>
